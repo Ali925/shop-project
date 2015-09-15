1c6a3ee9414c6b52feebbdd81b13a34505ec90a9
Maksim Ponomarenko <m.a.ponomarenko@yandex.ru>
2015-09-15 12:41:06 +0400

    Контроллер Products

---
+++ app/controllers/Controller_Products.php
@@ -0,0 +1,40 @@
+<?php
+//Контроллер для работы с каталогом
+class Controller_Products extends Controller
+{
+    public function __construct(){
+        parent::__construct();
+        $this->model = new Model_Products();
+    }
+//выводим все товары
+    function action_index(){
+        $this->view->generate('View_Products.php', 'View_Template.php',
+            array(
+                'title' => 'Список товаров',
+                'products' => $this->model->get_products()
+            )
+        );
+    }
+//Выводим данные по конкретной категории
+    function action_category($category){
+        $category = (string)$category;
+        $data = $this->model->get_category($category);
+        $this->view->generate('View_Category.php', 'View_Template.php',
+            array(
+                'title' => $category,
+                'products' => $data
+            )
+        );
+    }
+//Выводим данные по конкретному товару
+    function action_item($id){
+        $id = (int)$id[0];
+        $data = $this->model->get_item($id);
+        $this->view->generate('View_Item.php', 'View_Template.php',
+            array(
+                'title' => $data['title'],
+                'item' => $data
+            )
+        );
+    }
+}
