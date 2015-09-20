9771d42bfe38a0383a08cb08a9036e11e019c196
Ali925 <alitural.mehdi@gmail.com>
2015-09-20 21:18:45 +0500

    fix

--- application/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Подключаем базовые файлы
-require_once 'core/model.php';
-require_once 'core/view.php';
-require_once 'core/controller.php';
-require_once 'core/route.php';
-
-Route::start(); // запускаем маршрутизатор
